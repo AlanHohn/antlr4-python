60444127e06aef399bc233047d381b3a358b2d58
Alan Hohn <Alan.M.Hohn@lmco.com>
2016-03-29 22:36:27 -0400

    remove unnecessary walker

--- arithmetic/arithmetic.py
+++ arithmetic/arithmetic.py
@@ -4,45 +4,41 @@ from arithmeticParser import arithmeticParser
 import antlr4
 import sys
 
-class arithmeticPrintListener(arithmeticListener):
-
-    def enterExpression(self, ctx):
-        adding = True
-        value = 0
-        for child in ctx.getChildren():
-            if isinstance(child, antlr4.tree.Tree.TerminalNode):
-                adding = child.getText() == "+"
+def handleExpression(expr):
+    adding = True
+    value = 0
+    for child in expr.getChildren():
+        if isinstance(child, antlr4.tree.Tree.TerminalNode):
+            adding = child.getText() == "+"
+        else:
+            multValue = handleMultiply(child)
+            if adding:
+                value += multValue
             else:
-                multValue = self.handleMultiply(child)
-                if adding:
-                    value += multValue
-                else:
-                    value -= multValue
+                value -= multValue
 
-        print "Parsed expression %s has value %s" % (ctx.getText(), value)
+    print "Parsed expression %s has value %s" % (expr.getText(), value)
 
-    def handleMultiply(self, expr):
-        multiplying = True
-        value = 1
-        for child in expr.getChildren():
-            if isinstance(child, antlr4.tree.Tree.TerminalNode):
-                multiplying = child.getText() == "*"
+def handleMultiply(expr):
+    multiplying = True
+    value = 1
+    for child in expr.getChildren():
+        if isinstance(child, antlr4.tree.Tree.TerminalNode):
+            multiplying = child.getText() == "*"
+        else:
+            if multiplying:
+                value *= int(child.getText())
             else:
-                if multiplying:
-                    value *= int(child.getText())
-                else:
-                    value /= int(child.getText())
+                value /= int(child.getText())
 
-        return value
+    return value
 
 def main():
     lexer = arithmeticLexer(antlr4.StdinStream())
     stream = antlr4.CommonTokenStream(lexer)
     parser = arithmeticParser(stream)
     tree = parser.expression()
-    printer = arithmeticPrintListener()
-    walker = antlr4.ParseTreeWalker()
-    walker.walk(printer, tree)
+    handleExpression(tree)
 if __name__ == '__main__':
     main()
 
